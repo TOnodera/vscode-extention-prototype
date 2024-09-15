89d2ce16944409d99e1e82891ff36d0c9fa7a470
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-09-15 18:40:05 +0900

    なんとなくわっかた

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-//
-
-const cats = {
-  "Coding Cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
-  "Compiling Cat": "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
-};
-
 export function activate(context: vscode.ExtensionContext) {
-  let currentPanel: vscode.WebviewPanel | undefined = undefined;
   context.subscriptions.push(
     vscode.commands.registerCommand("catCoding.start", () => {
-      const columnToShowIn = vscode.window.activeTextEditor
-        ? vscode.window.activeTextEditor.viewColumn
-        : undefined;
+      const panel = vscode.window.createWebviewPanel(
+        "catCoding",
+        "Cat Coding",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        }
+      );
 
-      if (currentPanel) {
-        currentPanel.reveal(columnToShowIn);
-      } else {
-        currentPanel = vscode.window.createWebviewPanel(
-          "catCoding",
-          "Cat Coding",
-          vscode.ViewColumn.One
-        );
+      panel.webview.html = getWebviewContent();
 
-        currentPanel.webview.html = getWebviewContent("Coding Cat");
-
-        currentPanel.onDidDispose(
-          () => {
-            currentPanel = undefined;
-          },
-          null,
-          context.subscriptions
-        );
-      }
+      // Handle messages from the webview
+      panel.webview.onDidReceiveMessage(
+        (message) => {
+          switch (message.command) {
+            case "alert":
+              vscode.window.showErrorMessage(message.text);
+              return;
+          }
+        },
+        undefined,
+        context.subscriptions
+      );
     })
   );
 }
 
-function getWebviewContent(cat: keyof typeof cats) {
+function getWebviewContent() {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -51,10 +39,28 @@ function getWebviewContent(cat: keyof typeof cats) {
 	  <title>Cat Coding</title>
   </head>
   <body>
-	  <img src="${cats[cat]}" width="300" />
+	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+	  <h1 id="lines-of-code-counter">0</h1>
+  
+	  <script>
+		  (function() {
+			  const vscode = acquireVsCodeApi();
+			  const counter = document.getElementById('lines-of-code-counter');
+  
+			  let count = 0;
+			  setInterval(() => {
+				  counter.textContent = count++;
+  
+				  // Alert the extension when our cat introduces a bug
+				  if (Math.random() < 0.001 * count) {
+					  vscode.postMessage({
+						  command: 'alert',
+						  text: '🐛  on line ' + count
+					  })
+				  }
+			  }, 100);
+		  }())
+	  </script>
   </body>
   </html>`;
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
