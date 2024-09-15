be13c9e7f1c4871cfd7f181166980bf300a56d71
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-09-15 14:45:59 +0900

    猫切り替えまで

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,59 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+//
+
+const cats = {
+  "Coding Cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
+  "Compiling Cat": "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("catCoding.start", () => {
+      const panel = vscode.window.createWebviewPanel(
+        "catCoding",
+        "Cat Coding",
+        vscode.ViewColumn.One
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "simple-graphql-client" is now active!');
+      let iteration = 0;
+      const updateWebView = () => {
+        const cat = iteration++ % 2 ? "Compiling Cat" : "Coding Cat";
+        panel.title = cat;
+        panel.webview.html = getWebviewContent(cat);
+      };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('simple-graphql-client.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from simple-graphql-client!');
-	});
+      updateWebView();
 
-	context.subscriptions.push(disposable);
+      const interval = setInterval(updateWebView, 1000);
+
+      panel.onDidDispose(
+        () => {
+          clearInterval(interval);
+        },
+        null,
+        context.subscriptions
+      );
+    })
+  );
+}
+
+function getWebviewContent(cat: keyof typeof cats) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="${cats[cat]}" width="300" />
+  </body>
+  </html>`;
 }
 
 // This method is called when your extension is deactivated
